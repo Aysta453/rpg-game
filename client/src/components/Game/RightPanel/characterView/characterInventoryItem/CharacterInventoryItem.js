ba50aaffde413344769c39ed869c6fb0344812e9
Krzysztof Podkulski <krzysztof453vip@gmail.com>
2021-12-12 22:06:05 +0100

    inventory view update

--- client/src/components/Game/RightPanel/characterView/characterInventoryItem/CharacterInventoryItem.js
+++ client/src/components/Game/RightPanel/characterView/characterInventoryItem/CharacterInventoryItem.js
@@ -3,22 +3,32 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import showingEquipmentImage from '../../../../../functions/showingEquipmentImage';
 import CharacterPopup from './CharacterPopup';
-const CharacterInventoryItem = ({valueOfItem, specyficItem, slotNumber,sellThisItem,handleChangeItemIntoUse,size}) => {
+const CharacterInventoryItem = ({typeOfPosition ,valueOfItem, specyficItem, slotNumber,sellThisItem,handleChangeItemIntoUse,size}) => {
     
     const [popupValue, setPopupValueset] = useState(false);
     const hero = useSelector(state => state.hero);
     let image = showingEquipmentImage(hero.heroClass, specyficItem.typeOfItem, size, specyficItem.numberOfItem);
-
+    let id;
+    switch (typeOfPosition) {
+        case 1:
+            id="rightBorder";
+            break;
+        case 0:
+            id = "noneBorder";
+            break;
+        default:
+            break;
+    }
     const showPopup = () => {
         setPopupValueset(!popupValue);
     }
 
     return (valueOfItem) ? (
-        <div className="inventoryItem">
+        <div className="inventoryItem"  id={id}>
 
         </div>
     ) : (        
-            <div className='inventoryItem'>
+            <div className='inventoryItem' id={id}>
                 <img className="imageItem" src={`/images/eq${image}.png`} alt=""/>
                 <button className="itemInventory-btn" onClick={() => { showPopup() }}>+</button>
                 <CharacterPopup size={0} handleChangeItemIntoUse={handleChangeItemIntoUse}  slotNumber={slotNumber} sellThisItem={sellThisItem}  valueOfPoup={popupValue} showPopup={showPopup} specyficItem={specyficItem} />
