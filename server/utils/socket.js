6d60765156e2911a8a353fbae4cefef7d5bc11b6
Krzysztof Podkulski <krzysztof453vip@gmail.com>
2022-01-23 11:52:21 +0100

    Update socket.js

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -25,14 +25,8 @@ io.on("connection", function (socket) {
   socket.on("closeRoom", (roomName) => {
     io.in(roomName).emit("leaving", 2);
 
-    io.in(roomName).clients(function (error, clients) {
-      if (clients.length > 0) {
-        console.log("clients in the room: \n");
-        console.log(clients);
-        clients.forEach(function (socket_id) {
-          io.sockets.sockets[socket_id].leave(roomName);
-        });
-      }
+    socket.on("leave", (roomName) => {
+      socket.leave(roomName);
     });
   });
 
